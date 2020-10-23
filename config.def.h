feb0f3131d3e77c0996372d653e66f1dd2180585
Harsh Parekh <harsh_parekh@outlook.com>
2020-10-23 14:52:29 -0400

    Tune fonts and colors to fit my solarized theme.

--- config.def.h
+++ config.def.h
@@ -7,13 +7,13 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "TerminessTTF Nerd Font:size=10" };
+static const char *fonts[]          = { "TerminessTTF Nerd Font:size=11" };
 static const char dmenufont[]       = "TerminessTTF Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray3[]       = "#839496";
+static const char col_gray4[]       = "#93a1a1";
+static const char col_cyan[]        = "#002b36";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
