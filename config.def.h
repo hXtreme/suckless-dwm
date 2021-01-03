73da2117d8551d0883bf3c60b9a7a828dc1897ed
Harsh Parekh <harsh_parekh@outlook.com>
2021-01-02 21:32:52 -0500

    Update the icons for tags.

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "TerminessTTF Nerd Font:size=11" };
+static const char *fonts[]          = { "TerminessTTF Nerd Font:size=10" };
 static const char dmenufont[]       = "TerminessTTF Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
