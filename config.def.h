a3b859b43ae7d9c438c606275da982f2b09305c9
Harsh Parekh <harsh_parekh@outlook.com>
2021-07-21 00:24:35 -0400

    Center dmenu_run on screen

--- config.def.h
+++ config.def.h
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-b", "-p", " >", "-c", "-g", "3", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", " >", "-C", "-g", "4", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
