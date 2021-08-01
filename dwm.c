5449fb4ad5e2d0210f666cc0a53330411f454560
Harsh Parekh <harsh_parekh@outlook.com>
2021-08-01 14:23:39 -0400

    Apply patch dwm-cfacts-20200913-61bb8b2.diff

--- dwm.c
+++ dwm.c
@@ -92,6 +92,7 @@ typedef struct Client Client;
 struct Client {
 	char name[256];
 	float mina, maxa;
+	float cfact;
 	int x, y, w, h;
 	int oldx, oldy, oldw, oldh;
 	int basew, baseh, incw, inch, maxw, maxh, minw, minh;
@@ -224,6 +225,7 @@ static void setfocus(Client *c);
 static void setfullscreen(Client *c, int fullscreen);
 static void setgaps(const Arg *arg);
 static void setlayout(const Arg *arg);
+static void setcfact(const Arg *arg);
 static void setmfact(const Arg *arg);
 static void setup(void);
 static void seturgent(Client *c, int urg);
@@ -1154,6 +1156,7 @@ manage(Window w, XWindowAttributes *wa)
 	c->w = c->oldw = wa->width;
 	c->h = c->oldh = wa->height;
 	c->oldbw = wa->border_width;
+	c->cfact = 1.0;
 
 	updatetitle(c);
 	if (XGetTransientForHint(dpy, w, &trans) && (t = wintoclient(trans))) {
@@ -1827,6 +1830,23 @@ setlayout(const Arg *arg)
 		drawbar(selmon);
 }
 
+void setcfact(const Arg *arg) {
+	float f;
+	Client *c;
+
+	c = selmon->sel;
+
+	if(!arg || !c || !selmon->lt[selmon->sellt]->arrange)
+		return;
+	f = arg->f + c->cfact;
+	if(arg->f == 0.0)
+		f = 1.0;
+	else if(f < 0.25 || f > 4.0)
+		return;
+	c->cfact = f;
+	arrange(selmon);
+}
+
 /* arg > 1.0 will set mfact absolutely */
 void
 setmfact(const Arg *arg)
@@ -1990,9 +2010,15 @@ void
 tile(Monitor *m)
 {
 	unsigned int i, n, h, mw, my, ty;
+	float mfacts = 0, sfacts = 0;
 	Client *c;
 
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++) {
+		if (n < m->nmaster)
+			mfacts += c->cfact;
+		else
+			sfacts += c->cfact;
+	}
 	if (n == 0)
 		return;
 
@@ -2002,15 +2028,17 @@ tile(Monitor *m)
 		mw = m->ww - m->pertag->gaps[m->pertag->curtag].gappx;
 	for (i = 0, my = ty = m->pertag->gaps[m->pertag->curtag].gappx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
 		if (i < m->nmaster) {
-			h = (m->wh - my) / (MIN(n, m->nmaster) - i) - m->pertag->gaps[m->pertag->curtag].gappx;
+			h = (m->wh - my) * (c->cfact / mfacts) - m->pertag->gaps[m->pertag->curtag].gappx;
 			resize(c, m->wx + m->pertag->gaps[m->pertag->curtag].gappx, m->wy + my, mw - (2*c->bw) - m->pertag->gaps[m->pertag->curtag].gappx, h - (2*c->bw), 0);
 			if (my + HEIGHT(c) + m->pertag->gaps[m->pertag->curtag].gappx < m->wh)
 				my += HEIGHT(c) + m->pertag->gaps[m->pertag->curtag].gappx;
+     mfacts -= c->cfact;
 		} else {
-			h = (m->wh - ty) / (n - i) - m->pertag->gaps[m->pertag->curtag].gappx;
+			h = (m->wh - ty) * (c->cfact / sfacts) - m->pertag->gaps[m->pertag->curtag].gappx;
 			resize(c, m->wx + mw + m->pertag->gaps[m->pertag->curtag].gappx, m->wy + ty, m->ww - mw - (2*c->bw) - 2*m->pertag->gaps[m->pertag->curtag].gappx, h - (2*c->bw), 0);
 			if (ty + HEIGHT(c) + m->pertag->gaps[m->pertag->curtag].gappx < m->wh)
 				ty += HEIGHT(c) + m->pertag->gaps[m->pertag->curtag].gappx;
+     sfacts -= c->cfact;
 		}
 }
 
